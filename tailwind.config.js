d8994253a5b3157352d68ae3901246ce0323f883
Fran Swan <fran@boringprotocol.io>
2023-07-10 23:19:32 -0700

    add Inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
+        purple: '#221A2D',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
         btn: {
@@ -13,5 +19,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
