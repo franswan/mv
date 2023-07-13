207f03e53a15daef7495f78115318ecf6a2273ce
Fran Swan <fran@boringprotocol.io>
2023-07-12 21:38:11 -0700

    add favicon

--- app/build/mint/page.tsx
+++ app/build/mint/page.tsx
@@ -20,47 +20,47 @@ export default async function Index() {
             <p>It's Just that easy.</p>
           </div>
 
-          <ul className="timeline">
-            <li data-count="3">
-              <div className="left_content">
-                <div className="form-group asset-selection">
-                  <input type="file" name="assetselection" className="form-input" id="assetselection" />
-                  <label htmlFor="assetselection">Select your Asset</label>
-                </div>
-                <div className="form-group">
-                  <input type="text" className="form-input" placeholder="Estimated Value in USD" />
-                </div>
-                <div className="form-group">
-                  <input type="text" className="form-input" placeholder="Our Valuation" />
-                </div>
-                <div className="quickask">
-                  <p>Would you like to verify your valuation?</p>
-                  <div className="accredited-investor-group">
-                    <div className="form-group">
-                      <input type="radio" name="otherowner" id="yes" />
-                      <label htmlFor="yes">Yes</label>
-                    </div>
-                    <div className="form-group">
-                      <input type="radio" name="otherowner" id="no" />
-                      <label htmlFor="no">No</label>
-                    </div>
+          <div className="timeline ">
+
+            <div className="left_content space-y-6">
+              <div className="form-group asset-selection">
+                <input type="file" name="assetselection" className="input input-bordered w-full max-w-xs" id="assetselection" />
+                <label htmlFor="assetselection">Select your Asset</label>
+              </div>
+              <div className="form-group">
+                <input type="text" className="input input-bordered w-full max-w-xs" placeholder="Estimated Value in USD" />
+              </div>
+              <div className="form-group">
+                <input type="text" className="input input-bordered w-full max-w-xs" placeholder="Our Valuation" />
+              </div>
+              <div className="quickask">
+                <p>Would you like to verify your valuation?</p>
+                <div className="accredited-investor-group">
+                  <div className="form-group">
+                    <input type="radio" name="otherowner" id="yes" />
+                    <label htmlFor="yes">Yes</label>
+                  </div>
+                  <div className="form-group">
+                    <input type="radio" name="otherowner" id="no" />
+                    <label htmlFor="no">No</label>
                   </div>
                 </div>
-                <div className="form-group">
-                  <select className="form-select">
-                    <option selected>If Yes, Choose an Evaluator</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                  </select>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
               </div>
-              <div className="right_content">
-                <h3>Evaluation</h3>
+              <div className="form-group">
+                <select className="select input-bordered w-full max-w-xs">
+                  <option selected>If Yes, Choose an Evaluator</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
               </div>
-            </li>
-          </ul>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </div>
+            <div className="right_content">
+              <h3>Evaluation</h3>
+            </div>
+
+          </div>
         </div>
       </section>
 
